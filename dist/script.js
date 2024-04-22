cf5cdaa3677819579bb06b9424b4db4bfe037a82
ergeagrregr <165640284+ergeagrregr@users.noreply.github.com>
2024-04-22 15:16:32 +0800

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+<h2>計算長方形面積</h2>
+    <label for="length">長：</label>
+    <input type="number" id="length" placeholder="請輸入長" required><br><br>
+    <label for="width">寬：</label>
+    <input type="number" id="width" placeholder="請輸入寬" required><br><br>
+    <button onclick="calculateRectangleArea()">計算面積</button><br><br>
+    <div id="result"></div>
